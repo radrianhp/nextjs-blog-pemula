73310fce1e1c9ee03777d52cb4dddc7d5d3bdb21
Robertus Adrian <robertusadrian@gmail.com>
2023-02-27 22:00:15 +0700

    initial init

---
+++ pages/admin.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function admin() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Halo Admin Robertus</h1>
+      <a
+        href="#"
+        className="inline-flex px-5 ml-3 font-medium bg-indigo-600 rounded-md text-white hover:bg-indigo-900"
+      >
+        Button Click
+      </a>
+      <p>
+        {" "}
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+        molestias quibusdam soluta similique totam alias harum, autem itaque,
+        omnis a ea quis inventore, sequi ipsam rem non quaerat vel. Tenetur.
+      </p>
+    </div>
+  );
+}
---
+++ pages/item.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "../styles/Item.module.css";
+
+export default function item() {
+  return (
+    <div>
+      <h1 className={styles.font}>Halo Item</h1>
+    </div>
+  );
+}
