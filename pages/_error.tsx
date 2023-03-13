57a4152b2a3c5ed3d048f9e85efaec79e62f7536
Robertus Adrian <robertusadrian@gmail.com>
2023-03-13 20:33:20 +0700

    custom page

---
+++ pages/_error.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default function FallbackError() {
+  const refreshPage = () => window.location.reload();
+
+  return (
+    <div className='pt-16 pb-12 flex flex-col'>
+      <div className='flex-grow flex flex-col justify-center max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8'>
+        <div className='py-16'>
+          <div className='text-center'>
+            <h1 className='mt-2 text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl'>
+              Ups!
+            </h1>
+            <p className='mt-2 text-base text-gray-500'>Terjadi kesalahan.</p>
+            <div className='mt-6'>
+              <button
+                className='text-base font-medium text-blue-600'
+                onClick={refreshPage}
+                type='button'
+              >
+                Muat ulang halaman
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
